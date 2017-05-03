5d9c5626dbd9354b8ef147ea03225a350fcbe429
Shawn Mathew <shawn.mathew@agencychief.com>
2017-05-03 16:04:27 -0400

    add basic architecture, tmeplating

--- functions.php
+++ functions.php
@@ -3,9 +3,49 @@
 require_once 'inc/vendor/autoload.php';
 
 $loader = new Twig_Loader_Filesystem(get_stylesheet_directory() . '/views');
-$twig = new Twig_Environment($loader);
+$twig = new Twig_Environment($loader, array(
+	'debug' => true
+));
+$twig->addExtension(new Twig_Extension_Debug());
 
 
+/*
+* WP Functions
+*/
+$twig_wp_head = new Twig_Function('wp_head', function() {
+	return wp_head();
+});
+$twig->addFunction($twig_wp_head);
+
+$twig_wp_footer = new Twig_Function('wp_footer', function() {
+	return wp_footer();
+});
+$twig->addFunction($twig_wp_footer);
+
+$twig_css_directory = new Twig_Function('theme_path', function() {
+	echo get_stylesheet_directory_uri();
+});
+$twig->addFunction($twig_css_directory);
+
+$twig_site_url = new Twig_Function('site_url', function() {
+	echo get_site_url();
+});
+$twig->addFunction($twig_site_url);
+
+$twig_is_home = new Twig_Function('is_home', function() {
+	return is_front_page();
+});
+$twig->addFunction($twig_is_home);
+
+$twig_is_blog = new Twig_Function('is_blog', function() {
+	return is_page(602);
+});
+$twig->addFunction($twig_is_blog);
+
+
+/*
+* WP API Methods
+*/
 function rest_get_page($id) {			
 	$request = new WP_REST_Request('GET', '/wp/v2/pages/' . $id);
 	$response = rest_do_request($request);
@@ -14,14 +54,63 @@ function rest_get_page($id) {
 
 function rest_get_post($id) {
 	$request = new WP_REST_Request('GET', '/wp/v2/posts/' . $id);
-	$response = rest_get_server()->dispatch($request);
+	$response = rest_do_request($request);
 	return $response->data;	
 }
 
 function rest_get_all_posts() {
 	$request = new WP_REST_Request('GET', '/wp/v2/posts');
-	$response = rest_get_server()->dispatch($request);
+	$response = rest_do_request($request);
+	$responseArr = [];
+	$responseArr['data'] = $response->data;
+	$responseArr['total_pages'] = $response->headers['X-WP-TotalPages'];
+	return $responseArr;
+}
+
+function rest_get_paginated_posts($page_num) {
+	$request = new WP_REST_Request('GET', '/wp/v2/posts');
+	$request->set_param('page', $page_num);
+	$response = rest_do_request($request);
+	$responseArr = [];
+	$responseArr['data'] = $response->data;
+	$responseArr['total_pages'] = $response->headers['X-WP-TotalPages'];
+	return $responseArr;
+}
+
+function rest_get_limited_posts($count) {
+	$request = new WP_REST_Request('GET', '/wp/v2/posts');
+	$request->set_param('per_page', $count);
+	$response = rest_do_request($request);
+	return $response->data;
+}
+
+function rest_get_sidebar($sidebar) {
+	$request = new WP_REST_Request('GET', '/wp-rest-api-sidebars/v1/sidebars/' . $sidebar);
+	$response = rest_do_request($request);
 	return $response->data;	
 }
+
+function rest_get_header_menu() {
+	$request = new WP_REST_Request('GET', '/wp-api-menus/v2/menus/2');
+	$response = rest_do_request($request);
+	return $response->data;	
+}
+
+function rest_get_footer_menu() {
+	$request = new WP_REST_Request('GET', '/wp-api-menus/v2/menus/3');
+	$response = rest_do_request($request);
+	return $response->data;	
+}
+
+/*
+* Utility Functions
+*/
+function blog_list_pagination_num() {
+	$req_params = $_SERVER['REQUEST_URI'];
+	$page = preg_replace('/[^0-9]/', '', $req_params);
+	return $page;
+}
+
+
 
 ?>
--- page.php
+++ page.php
@@ -1,16 +1,30 @@
 <?php
 
+
 global $post;
-//var_dump($post);
+$page_num = blog_list_pagination_num();
 
-
-$sb = dynamic_sidebar('header-right');
 
 $context = array();
 $context['title'] = $post->post_name;
-$context['restdata'] = rest_get_page($post->ID);
-$context['blog_list'] = is_page(602) ? rest_get_all_posts() : '';
-$context['home_sidebar_right'] = $sb;
+$context['page_data'] = rest_get_page($post->ID);
+$context['home_sidebar_right'] = rest_get_sidebar('header-right');
+$context['page_num'] = $page_num;
+
+$context['header_menu'] = rest_get_header_menu();
+$context['footer_menu'] = rest_get_footer_menu();
+
+
+
+
+if( is_page(602) ) {
+	$context['blog_list'] = !empty($page_num) ? rest_get_paginated_posts($page_num) : rest_get_all_posts();
+}
+
+if( is_front_page() ) {
+	$context['recent_posts'] = rest_get_limited_posts(4);
+	$context['home_cta'] = rest_get_sidebar('footer-2');
+}
 
 
 echo $twig->render('page.twig', $context );
