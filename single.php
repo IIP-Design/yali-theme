a2166f2c0fd77d325f26aed7ad2fddea6365f3f8
Shawn Mathew <shawn.mathew@agencychief.com>
2017-05-03 16:45:32 -0400

    add footer menu on single posts

--- single.php
+++ single.php
@@ -6,4 +6,7 @@ $context = array();
 $context['title'] = $post->post_name;
 $context['restdata'] = rest_get_post($post->ID);
 
+$context['header_menu'] = rest_get_header_menu();
+$context['footer_menu'] = rest_get_footer_menu();
+
 echo $twig->render('single.twig', $context);
