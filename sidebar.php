2ca90bb3654a27088ddd16683c268c274f7fd6a3
Shawn Mathew <shawn.mathew@agencychief.com>
2017-05-05 10:05:48 -0400

    remove sidebar.php

--- sidebar.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-$context = array();
-
-// $context['home_sidebar_right'] = Yali::get_sidebar('header-right');
-// $context['blog_sidebar'] = Yali::get_sidebar('primary-sidebar');
-
-echo $twig->render('sidebar.twig', $context);
