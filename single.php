3290e9d905430c0d63e98a8d807bf9b77af09bf0
mrewers <marekrewers@gmail.com>
2017-07-13 10:27:29 -0400

    Creating single.php and single.twig

--- single.php
+++ single.php
@@ -1,19 +1,19 @@
 <?php
 
+use Yali\Twig as Twig;
+
 // Post Object
 global $post;
 
-// Main data array to pass to template
-$context = array();
+$post_data = Yali\API::get_post($post->ID);
+$tags = get_the_tags($post->ID);
+$categories = get_the_category($post->ID);
 
-// Populate array w/ elements needed for page
-$context['post_data'] = Yali::get_post($post->ID);
-$context['header_menu'] = Yali::get_header_menu();
-$context['footer_menu'] = Yali::get_footer_menu();
-$context['sidebar'] = Yali::get_sidebar('primary-sidebar');
-$context['tags'] = get_the_tags($post->ID);
-$context['categories'] = get_the_category($post->ID);
-
+$context = array(
+  'post_data' => $post_data,
+  'tags' => $tags,
+  'categories' => $categories
+);
 
 // Render template passing in data array
-echo $twig->render('single.twig', $context);
+echo Twig::render('single.twig', $context);
