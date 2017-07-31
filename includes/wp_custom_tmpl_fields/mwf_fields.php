57eaea7c11dfe7b6d80e3363bf849cde18afdc57
Shawn Mathew <shawn.mathew@agencychief.com>
2017-07-31 17:53:21 -0400

    add custom fields in admin, sections in template

--- includes/wp_custom_tmpl_fields/mwf_fields.php
+++ includes/wp_custom_tmpl_fields/mwf_fields.php
@@ -39,11 +39,26 @@ function mwf_fields() {
     	'priority'     => 'high',
     	'show_on'      => array('key' => 'page-template', 'value' => 'page-templates/mwf.php'),
     	'show_in_rest' => true
-	) );
+	) );	
 
-	$mwf_intro->add_field( array(    	
-	    'id'      => $prefix . 'intro_content',
-	    'type'    => 'wysiwyg',
+    $mwf_intro->add_field( array(    	
+    	'name'    => 'Enter Paragraph 1 Text',
+	    'id'      => $prefix . 'intro_content_txt1',
+	    'type'    => 'textarea',
+	    'options' => array( 'textarea_rows' => 10, )
+    ) );
+
+    $mwf_intro->add_field( array(    	
+    	'name'    => 'Enter Paragraph 2 Text (Optional)',
+	    'id'      => $prefix . 'intro_content_txt2',
+	    'type'    => 'textarea',
+	    'options' => array( 'textarea_rows' => 10, )
+    ) );
+
+    $mwf_intro->add_field( array(    	
+    	'name'    => 'Enter Paragraph 3 Text (Optional)',
+	    'id'      => $prefix . 'intro_content_txt3',
+	    'type'    => 'textarea',
 	    'options' => array( 'textarea_rows' => 10, )
     ) );
 
@@ -56,5 +71,107 @@ function mwf_fields() {
     	'priority'     => 'high',
     	'show_on'      => array('key' => 'page-template', 'value' => 'page-templates/mwf.php'),
     	'show_in_rest' => true
-	) );	
+	) );
+
+	$mwf_section1->add_field( array(    	
+    	'name'    => 'Enter Section 1 Title',
+	    'id'      => $prefix . 'section1_title',
+	    'type'    => 'text'	    
+    ) );
+
+	$mwf_section1->add_field( array(    	
+    	'name'    => 'Enter Paragraph 1 Text',
+	    'id'      => $prefix . 'section1_content_txt1',
+	    'type'    => 'textarea',
+	    'options' => array( 'textarea_rows' => 10, )
+    ) );	
+
+	$mwf_section1->add_field( array(    	
+    	'name'    => 'Enter Paragraph 2 Text (Optional)',
+	    'id'      => $prefix . 'section1_content_txt2',
+	    'type'    => 'textarea',
+	    'options' => array( 'textarea_rows' => 10, )
+    ) );
+
+	$mwf_section1->add_field( array(    	
+    	'name'    => 'Enter Paragraph 3 Text (Optional)',
+	    'id'      => $prefix . 'section1_content_txt3',
+	    'type'    => 'textarea',
+	    'options' => array( 'textarea_rows' => 10, )
+    ) );
+
+    // Section 2
+    $mwf_section2 = new_cmb2_box( array(
+		'id'           =>  $prefix . 'section2',
+    	'title'        => __( 'Enter Section 2 Content', 'mwf' ),
+    	'object_types' => array( 'page' ),
+    	'context'      => 'advanced',
+    	'priority'     => 'high',
+    	'show_on'      => array('key' => 'page-template', 'value' => 'page-templates/mwf.php'),
+    	'show_in_rest' => true
+	) );
+
+	$mwf_section2->add_field( array(    	
+    	'name'    => 'Enter Section 2 Title',
+	    'id'      => $prefix . 'section2_title',
+	    'type'    => 'text'	    
+    ) );
+
+	$mwf_section2->add_field( array(    	
+    	'name'    => 'Enter Paragraph 1 Text',
+	    'id'      => $prefix . 'section2_content_txt1',
+	    'type'    => 'textarea',
+	    'options' => array( 'textarea_rows' => 10, )
+    ) );	
+
+	$mwf_section2->add_field( array(    	
+    	'name'    => 'Enter Paragraph 2 Text (Optional)',
+	    'id'      => $prefix . 'section2_content_txt2',
+	    'type'    => 'textarea',
+	    'options' => array( 'textarea_rows' => 10, )
+    ) );
+
+	$mwf_section2->add_field( array(    	
+    	'name'    => 'Enter Paragraph 3 Text (Optional)',
+	    'id'      => $prefix . 'section2_content_txt3',
+	    'type'    => 'textarea',
+	    'options' => array( 'textarea_rows' => 10, )
+    ) );
+
+    // Application Section
+    $mwf_application = new_cmb2_box( array(
+		'id'           =>  $prefix . 'application',
+    	'title'        => __( 'Enter Application Content', 'mwf' ),
+    	'object_types' => array( 'page' ),
+    	'context'      => 'advanced',
+    	'priority'     => 'high',
+    	'show_on'      => array('key' => 'page-template', 'value' => 'page-templates/mwf.php'),
+    	'show_in_rest' => true
+	) );
+
+	$mwf_application->add_field( array(    	
+    	'name'    => 'Enter Application Section Title',
+	    'id'      => $prefix . 'application_title',
+	    'type'    => 'text'	    
+    ) );
+
+    $mwf_application->add_field( array(
+    	'name'    => 'Application Process Timeline',
+	    'id'      => $prefix . 'application_process_timeline',
+	    'type'    => 'textarea',
+	    'options' => array( 'textarea_rows' => 10, )
+    ) );
+
+    $mwf_application->add_field( array(
+    	'name'    => 'Application Selection Process',
+	    'id'      => $prefix . 'application_selection_process',
+	    'type'    => 'textarea',
+	    'options' => array( 'textarea_rows' => 10, )
+    ) );
+
+    $mwf_application->add_field( array(
+    	'name'    => 'Who Is Eligible to Apply Section',
+	    'id'      => $prefix . 'application_eligibility',
+	    'type'    => 'wysiwyg'	    
+    ) );
 }
