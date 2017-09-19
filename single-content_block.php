2990641d1037995fb6f895d9da6b281e32b33721
Shawn Mathew <shawn.mathew@agencychief.com>
2017-09-19 11:54:53 -0400

    add content block preview

---
+++ single-content_block.php
@@ -0,0 +1,19 @@
+<?php
+
+use Yali\Twig as Twig;
+
+// Post Object
+global $post;
+
+$post_data = Yali\API::get_contentblock($post->ID);
+$content_block = do_shortcode($post_data['display_shortcode']);
+
+
+$context = array(  
+  'post_data'       => $post_data,
+  'header_url'      => $header_url,
+  'content_block'   => $content_block
+);
+
+// Render template passing in data array
+echo Twig::render('single-content_block.twig', $context);
