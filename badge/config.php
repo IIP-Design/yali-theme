e4ce80222d6ac9ac880939b11b5c7cba6c85c64a
mrewers <marekrewers@gmail.com>
2017-10-20 14:58:39 -0400

    Adding courses nonce, adjusting text size/alignment on existing badges

--- badge/config.php
+++ badge/config.php
@@ -59,32 +59,33 @@ return array(
 		'font'	  				=> 'fonts/Futuri Condensed Extra Bold.ttf',
 		'font_size'				=> 50,
 		'color'						=> '#314071',
-		'align'						=> 'center',
+		'align'						=> 'left',
 		'line_max_chars' 	=> 40,
 		'line_height' 		=> 42,
 		'text'						=> array (
 			array (
 				'content' 		=> 'My name is',     	 // Static content
-				'x'		  			=> 150,
-				'y'		  			=> 150
+				'x'		  			=> 50,
+				'y'		  			=> 70
 			),
 			array (
 				'content' 		=> 'FIELD',
 				'field_id'		=> 'pledge_name', 			// Pledge name (formidable field key) -- MUST match
-				'x'		  			=> 450,
-				'y'		  			=> 150,
+				'x'		  			=> 300,
+				'y'		  			=> 68,
 				'color'				=> '#58afd5'
 			),
 			array (
-				'content' 		=> 'and I pledge to',		// Static content
-				'x'		  			=> 180,
-				'y'		  			=> 230
+				'content' 		=> 'and I pledge to:',		// Static content
+				'x'		  			=> 50,
+				'y'		  			=> 150
 			),
 			array (
 				'content' 		=> 'FIELD',
 				'field_id'		=> 'pledge',
-				'x'		  			=> 220,
-				'y'		  			=> 310
+				'x'		  			=> 50,
+				'y'		  			=> 220,
+				'color'				=> '#58afd5'
 			)
 		)
 	),
@@ -95,21 +96,21 @@ return array(
 		'font'	  				=> 'fonts/Lato-Regular.ttf',
 		'font_size'				=> 32,
 		'color'						=> '#bbd485',
-		'align'						=> 'center',
+		'align'						=> 'left',
 		'line_max_chars' 	=> 40,
 		'line_height' 		=> 40,
 		'text'						=> array (
 			array (
 				'content' 		=> 'FIELD',
 				'field_id'		=> 'first_name', 			// First name (formidable field key) -- MUST match
-				'x'		  			=> 275,
+				'x'		  			=> 175,
 				'y'		  			=> 360
 			),
 			array (
 				'content' 		=> 'FIELD',
 				'field_id'		=> 'last_name', 	// Last Name (formidable field key)  -- MUST match
-				'x'		  			=> 275,
-				'y'		  			=> 400
+				'x'		  			=> 175,
+				'y'		  			=> 410
 			),
 			array (
 				'content' 		=> 'FIELD',      // Country (formidable field key)  -- MUST match
@@ -117,14 +118,14 @@ return array(
 				'align'				=> 'left',
 				'color'				=> '#06693a',
 				'font-size'		=> 20,
-				'x'		  			=> 275,
-				'y'		  			=> 436
+				'x'		  			=> 175,
+				'y'		  			=> 460
 			)
 		)
 	),
 	'get_yalilearns2016' 	=>  array (   					// Form that is sending certificate -- MUST match form key
 		'prefix'					=> 'yali_learns_',
-		'src_path'				=> 'images/ ',
+		'src_path'				=> 'images/yali_learns_pledge_bkgrd.png',
 		'save_path'				=> '../../../uploads/generated-images/',
 		'font'	  				=> 'fonts/AvenirNextCondensed-Heavy.ttf',
 		'font_size'				=> 44,
@@ -135,24 +136,24 @@ return array(
 		'text'						=> array (
 			array (
 				'content' 		=> 'FIELD',
+				'field_id'		=> 'challenge',      // challenge
+				'x'		  			=> 428,
+				'y'		  			=> 270,
+				'color'				=> '#bf202a',
+				'font_size'		=> 30,
+				'font'				=> 'fonts/AvenirNext-Bold.ttf'
+			),
+			array (
+				'content' 		=> 'FIELD',
 				'field_id'		=> 'full_name', 			// Full name (formidable field key) -- MUST match
 				'x'		  			=> 428,
-				'y'		  			=> 250
+				'y'		  			=> 350
 			),
 			array (
 				'content' 		=> 'FIELD',
 				'field_id'		=> 'country2', 	// Country (formidable field key)  -- MUST match
 				'x'		  			=> 428,
-				'y'		  			=> 385
-			),
-			array (
-				'content' 		=> 'FIELD',
-				'field_id'		=> 'challenge',      // Date, static field
-				'x'		  			=> 428,
-				'y'		  			=> 426,
-				'font_size' 	=> 24,
-				'color'				=> '#bf202a',
-				'font'				=> 'fonts/AvenirNext-Bold.ttf'
+				'y'		  			=> 410
 			)
 		)
 	),
@@ -171,7 +172,7 @@ return array(
 				'content' 		=> 'FIELD',
 				'field_id'		=> 'course_name2', 			// Course name (formidable field key) -- MUST match
 				'x'		  			=> 550,
-				'y'		  			=> 380
+				'y'		  			=> 360
 			),
 			array (
 				'content' 		=> 'FIELD',
@@ -188,36 +189,35 @@ return array(
 			)
 		)
 	),
-	'get_4all2' 	=>  array (   					// Form that is sending certificate -- MUST match form key
+	'get_4all' 	=>  array (   					// Form that is sending certificate -- MUST match form key
 		'prefix'					=> 'yali_4all_',
 		'src_path'				=> 'images/yali_4all_bkgrd.png',
 		'save_path'				=> '../../../uploads/generated-images/',
 		'font'	  				=> 'fonts/avenir-light-webfont.ttf',
-		'font_size'				=> 40,
+		'font_size'				=> 50,
 		'color'						=> '#ffffff',
 		'align'						=> 'center',
-		'line_max_chars' 	=> 40,
+		'line_max_chars' 	=> 50,
 		'line_height' 		=> 42,
 		'text'						=> array (
 			array (
 				'content' 		=> 'FIELD',
 				'field_id'		=> 'full_name2', 			// Full name (formidable field key) -- MUST match
 				'x'		  			=> 600,
-				'y'		  			=> 410,
-				'font_size'		=> 50
+				'y'		  			=> 400,
+				'font_size'		=> 55
 			),
 			array (
 				'content' 		=> 'FIELD',
-				'field_id'		=> 'country3', 	// Country (formidable field key)  -- MUST match
+				'field_id'		=> 'challenge2', // Challenge (formidable field key)  -- MUST match
 				'x'		  			=> 285,
 				'y'		  			=> 650
 			),
 			array (
 				'content' 		=> 'FIELD',
-				'field_id'		=> 'challenge2', // Challenge (formidable field key)  -- MUST match
+				'field_id'		=> 'country3', 	// Country (formidable field key)  -- MUST match
 				'x'		  			=> 925,
-				'y'		  			=> 650,
-				'font_size' 	=> 22
+				'y'		  			=> 650
 			)
 		)
 	)
