9a727141cb0f247051374086e771b564186c14f5
Shawn Mathew <shawn.mathew@agencychief.com>
2017-10-26 11:33:44 -0400

    add hero title display option, edit related styling

--- page.php
+++ page.php
@@ -23,14 +23,15 @@ $sizes = wp_get_attachment_image_sizes($img_id, "full");
 $categories = Yali\API::get_category_list();
 $series = get_terms('series');
 
-
 // Data for certain pages or shared
 $social_block = do_shortcode("[content_block id='13313']");
 
+// 'Join the Network' Form
 $formVar = do_shortcode('[formidable id=6]');
 
-// Yali Learns - Campaign Materials Accordion
-$campaign_materials_accordion = do_shortcode("[content_block id='13615' title='Yali Learns Campaign Materials']");
+// Hero Title Display
+$hero_title_display = get_post_meta($post->ID, 'hero_title_option', true);
+
 
 if( $pagename === 'action' ) {
   $campaigns = ( $check_host == 'yali.dev.america.gov' ) ? Yali\API::get_child_pages(13240) : Yali\API::get_child_pages(8);
@@ -46,12 +47,12 @@ $context = array(
   "feat_img"      => $feat_img_obj,
   "srcset"		    => $srcset,
   "sizes"		      => $sizes,
+  "hero_title_display" => $hero_title_display,
   "social_block"  => $social_block,
   "formVar"       => $formVar,
   'category_list' => $categories,
-  'series_list'   => $series,
-  'campaign_materials_accordion'  => $campaign_materials_accordion,
-  "campaigns"       => ( $pagename === 'action' ) ? $campaigns : null
+  'series_list'   => $series,  
+  "campaigns"     => ( $pagename === 'action' ) ? $campaigns : null
 );
 
 echo Twig::render( array( "pages/page-" . $pagename . ".twig", "page.twig" ), $context );
