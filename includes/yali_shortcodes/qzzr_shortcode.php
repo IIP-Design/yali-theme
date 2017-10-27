c82ffd83b73e95c59882b5264d1e33f2083bb69f
Shawn Mathew <shawn.mathew@agencychief.com>
2017-10-27 09:53:29 -0400

    edit qzzr shortcode attrs

--- includes/yali_shortcodes/qzzr_shortcode.php
+++ includes/yali_shortcodes/qzzr_shortcode.php
@@ -5,13 +5,13 @@ function qzzr_embed( $atts ) {
 	extract( 
 		shortcode_atts( 
 			array(
-				'quiz_id' => '',
-				'quiz_title' => ''
+				'id' => '',
+				'title' => ''
 			), 
 		$atts ) 
 	);
 
-	$qzzr_html = '<div class="quizz-container" data-width="100%" data-iframe-title="' . $quiz_title . '" data-height="auto" data-quiz="' . $quiz_id . '"></div>';
+	$qzzr_html = '<div class="quizz-container" data-width="100%" data-iframe-title="' . $title . '" data-height="auto" data-quiz="' . $id . '"></div>';
 
 	wp_enqueue_script('qzzr_js','http://dcc4iyjchzom0.cloudfront.net/widget/loader.js', '', '', true);
 
