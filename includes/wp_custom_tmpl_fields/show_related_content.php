fe8e46defa18e4b6dd36fcaf063a3de039e0c1d5
mrewers <marekrewers@gmail.com>
2017-10-30 19:39:47 -0400

    Adding show related content metabox

---
+++ includes/wp_custom_tmpl_fields/show_related_content.php
@@ -0,0 +1,31 @@
+<?php
+
+// Add option to include page title as overlay in hero section otherwise default to
+// under hero (in .ui.container)
+
+add_action('cmb2_init', 'related_content_option');
+function related_content_option() {
+
+	$hero_title  = new_cmb2_box( array(
+		'id'           =>  'related_content',
+    	'title'        => __( 'Related content', 'default-post' ),
+    	'object_types' => array( 'post' ),
+    	'context'      => 'normal',
+    	'priority'     => 'high',
+    	'show_in_rest' => true
+	) );
+
+	$hero_title->add_field( array(
+		'name' => '',
+		'id'   => 'related_content_option',
+		'desc' => 'Show additional content related to this story.',
+		'type' => 'radio_inline',
+		'options' => array(
+			'yes'   => __( 'Yes', 'default-post' ),
+			'no'    => __( 'No', 'default-post' )
+		),
+		'default' => 'no'
+	));
+
+
+}
