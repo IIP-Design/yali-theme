2b9929ff2ad1fd68f806ccc68ad2750c7633e55b
Shawn Mathew <shawn.mathew@agencychief.com>
2017-10-30 17:15:26 -0400

    edit search results page layout, filter search results

--- search.php
+++ search.php
@@ -2,15 +2,38 @@
 
 use Yali\Twig as Twig;
 
-//global $query_string;
+// global $query_string;
 global $wp_query;
 
 $search_term = get_search_query();
 $search_results = $wp_query->posts;
 
+// Formate search results to include an excerpt derived from post/page content
+// exclusive of shortcodes, images
+foreach ($search_results as $post) {
+	$content = $post->post_content;
+	$post->yali_excerpt = formatted_excerpt($content);
+}
 
-var_dump($wp_query);
+//var_dump($search_results);
 
+function formatted_excerpt($post_content) {
+	$word_length = 35;
+	
+	$excerpt = $post_content;
+	$excerpt = strip_tags(strip_shortcodes($excerpt));
+
+	$words = explode(' ', $excerpt, $word_length + 1);
+
+	if( count($words) > $word_length ) {
+		array_pop($words);
+		array_push($words, '...');		
+	}
+
+	$the_excerpt = implode(' ', $words);
+
+	return $the_excerpt;
+}
 
 // 'Join the Network' Form
 $formVar = do_shortcode('[formidable id=6]');
