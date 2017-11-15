f9dd9b166226770bc12dbf68a6d9c698af186ca6
mrewers <marekrewers@gmail.com>
2017-11-15 13:15:52 -0500

    Removing default values from social block and setting buttons to visible only if populated

--- includes/content_block_metaboxes/social.php
+++ includes/content_block_metaboxes/social.php
@@ -7,7 +7,7 @@ $cb_social_links = new_cmb2_box( array(
   'title'        => __( 'Social Media Links', 'yali' ),
   'object_types' => array( 'content_block' ),
   'priority'     => 'low'
-));  
+));
 
 $cb_social_links->add_field( array(
   'name'    => 'Title',
@@ -19,20 +19,17 @@ $cb_social_links->add_field( array(
 $cb_social_links->add_field( array(
   'name'    => 'Facebook',
   'id'      => $prefix . 'cb_social_links_facebook',
-  'type'    => 'text',
-  'default' => 'https://www.facebook.com/YALINetwork'
+  'type'    => 'text'
 ));
 
 $cb_social_links->add_field( array(
   'name'    => 'Twitter',
   'id'      => $prefix . 'cb_social_links_twitter',
-  'type'    => 'text',
-  'default' => 'https://twitter.com/YALINetwork'
+  'type'    => 'text'
 ));
 
 $cb_social_links->add_field( array(
   'name'    => 'LinkedIn',
   'id'      => $prefix . 'cb_social_links_linkedin',
-  'type'    => 'text',
-  'default' => 'https://www.linkedin.com/groups/7425359/profile'
+  'type'    => 'text'
 ));
