9dbab9a053b165f5b47461d523ec6caac581cdc1
Shawn Mathew <shawn.mathew@agencychief.com>
2017-11-20 12:50:04 -0500

    button links content block updates

--- includes/content_block_metaboxes/button-links.php
+++ includes/content_block_metaboxes/button-links.php
@@ -8,10 +8,17 @@ $button_links = new_cmb2_box( array(
   'priority'     => 'low'
 ));
 
+$button_links->add_field( array(
+	'name'    => 'Button Links Block Headline',
+	'id'      => $prefix . 'button_links_headline',
+	'description' => __('Add headline to be displayed above button links (Optional)'),
+	'type'    => 'text'
+));
+
 $button_links_group_field = $button_links->add_field( array(
-	'id'		=>	'button_links_repeat_group',
+	'id'		=>	$prefix . 'button_links_repeat_group',
 	'type'  	=> 'group',
-	'description'   => __( 'Add Button Link Item' ),
+	'description'   => __( 'Add Button Link Items - Add either a file or a link for button, include button text' ),
 	'options'       => array(
 	    'group_title'     => __( 'Button Link Item {#}', 'yali' ),
 	    'add_button'      => __( 'Add Another Button Link Item', 'yali' ),
@@ -21,8 +28,14 @@ $button_links_group_field = $button_links->add_field( array(
 ));
 
 $button_links->add_group_field( $button_links_group_field, array(
+ 	'name'  	=> 'Add text for button file display',
+	'id'    	=> $prefix . 'button_file_text',
+	'type'  	=> 'text',	
+));
+
+$button_links->add_group_field( $button_links_group_field, array(
  	'name'  	=> 'Add file to link to',
-	'id'    	=> 'link_file',
+	'id'    	=> $prefix . 'button_file',	
 	'type'  	=> 'file',
 	'options'	=> array(
 		'url'	=> false
@@ -31,8 +44,15 @@ $button_links->add_group_field( $button_links_group_field, array(
 
 $button_links->add_group_field( $button_links_group_field, array(
 	'name'                      => __( 'Add link for button', 'yali' ),
-	'id'                        => $prefix . 'cdp_autocomplete_related',
-	'type'                      => 'related_link',
+	'id'                        => $prefix . 'button_link',
+	'type'                      => 'link_picker',
 	'desc'                      => __( '' ),
 	'repeatable'                => false	
+));
+
+$button_links->add_group_field( $button_links_group_field, array(
+	'name'                      => __( 'Check if link is on same page', 'yali' ),
+	'desc'                      => __( 'adds a class to scroll to link if link is on same page' ),
+	'id'                        => $prefix . 'button_link_scroll',
+	'type'                      => 'checkbox',	
 ));
