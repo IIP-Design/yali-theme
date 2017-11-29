8a00e6b1340220b1ad10358f1e0b3c82155d5ccb
mrewers <marekrewers@gmail.com>
2017-11-29 12:58:02 -0500

    Connecting AddThis to Google Analytics

--- functions.php
+++ functions.php
@@ -5,86 +5,6 @@
  */
 require_once get_stylesheet_directory() . '/includes/autoloader.php';
 
-/*
- * Inserts Google Analytics snippet into head
- */
-add_action('wp_head', 'insert_google_analytics');
-function insert_google_analytics() {
-	?>
-		<!-- Google Analytics -->
-		<script>
-		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-		ga('create', 'UA-22995010-23', 'auto');
-		ga('send', 'pageview');
-		</script>
-		<!-- End Google Analytics -->
-	<?php
-}
-
-/*
- * Inserts Google Tag Manager snippets into head and body
- */
-add_action('wp_head', 'insert_gtm_head');
-function insert_gtm_head() {
-	?>
-		<!-- Google Tag Manager -->
-		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-		})(window,document,'script','dataLayer','GTM-TTR686');</script>
-		<!-- End Google Tag Manager -->
-	<?php
-}
-
-add_action('tha_body_top', 'insert_gtm_body');
-function insert_gtm_body() {
-	?>
-	<!-- Google Tag Manager (noscript) -->
-	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TTR686"
-	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-	<!-- End Google Tag Manager (noscript) -->
-	<?php
-}
-
-/**
- * Inserts Digital Analytics Program (DAP) code
- */
-add_action('wp_head', 'insert_dap');
-function insert_dap(){
-	?>
-	  <!-- Digital Analytics Program -->
-		<script async type="text/javascript" src="https://dap.digitalgov.gov/Universal-Federated-Analytics-Min.js?agency=DOS&siteplatform=YALI" id="_fed_an_ua_tag"></script>
-		<!-- End Digital Analytics Program -->
-	<?php
-}
-
-/**
- * Insert Hotjar Tagging
- *
-**/
-add_action('wp_head', 'insert_hotjar');
-function insert_hotjar() {
-	?>
-	<!-- Hotjar Tracking  -->
-	<script>
-	    (function(h,o,t,j,a,r){
-	        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-	        h._hjSettings={hjid:690455,hjsv:6};
-	        a=o.getElementsByTagName('head')[0];
-	        r=o.createElement('script');r.async=1;
-	        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-	        a.appendChild(r);
-	    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-	</script>
-	<!-- End Hotjar Tracking  -->
-	<?php
-}
-
 
 /**
  * Require badge generation class
@@ -214,6 +134,11 @@ class YaliSite {
 		add_action( 'pre_get_posts', array( $this, 'search_filter') );
 		add_action( 'tha_body_bottom', array( $this, 'insert_addthis_snippet') );
 		add_filter( 'wpseo_metabox_prio', array( $this, 'yoasttobottom' ) );
+		add_action( 'wp_head', array( $this, 'insert_google_analytics') );
+		add_action( 'wp_head', array( $this, 'insert_gtm_head') );
+		add_action( 'tha_body_top', array( $this, 'insert_gtm_body') );
+		add_action( 'wp_head', array( $this, 'insert_dap') );
+		add_action( 'wp_head', array( $this, 'insert_hotjar') );
 
 		$this->twig_init();
 
@@ -370,13 +295,88 @@ class YaliSite {
     return 'low';
   }
 
-	// Adds social share button from Addthis
+	/* ANALYTICS */
+	// Inserts Google Analytics snippet into head
+	function insert_google_analytics() {
+		?>
+			<!-- Google Analytics -->
+			<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+			ga('create', 'UA-22995010-23', 'auto');
+			ga('send', 'pageview');
+			</script>
+			<!-- End Google Analytics -->
+		<?php
+	}
+
+	// Inserts Google Tag Manager snippets into head and body
+	function insert_gtm_head() {
+		?>
+			<!-- Google Tag Manager -->
+			<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+			new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+			j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+			'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+			})(window,document,'script','dataLayer','GTM-TTR686');</script>
+			<!-- End Google Tag Manager -->
+		<?php
+	}
+
+	function insert_gtm_body() {
+		?>
+		<!-- Google Tag Manager (noscript) -->
+		<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TTR686"
+		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+		<!-- End Google Tag Manager (noscript) -->
+		<?php
+	}
+
+	// Inserts Digital Analytics Program (DAP) code
+	function insert_dap(){
+		?>
+		  <!-- Digital Analytics Program -->
+			<script async type="text/javascript" src="https://dap.digitalgov.gov/Universal-Federated-Analytics-Min.js?agency=DOS&siteplatform=YALI" id="_fed_an_ua_tag"></script>
+			<!-- End Digital Analytics Program -->
+		<?php
+	}
+
+	// Insert Hotjar Tagging
+	function insert_hotjar() {
+		?>
+		<!-- Hotjar Tracking  -->
+		<script>
+		    (function(h,o,t,j,a,r){
+		        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+		        h._hjSettings={hjid:690455,hjsv:6};
+		        a=o.getElementsByTagName('head')[0];
+		        r=o.createElement('script');r.async=1;
+		        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+		        a.appendChild(r);
+		    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+		</script>
+		<!-- End Hotjar Tracking  -->
+		<?php
+	}
+
+	// Adds social share button from Addthis and connects to Google Analytics
 	function insert_addthis_snippet() {
 		?>
 		<!-- Go to www.addthis.com/dashboard to customize your tools -->
+		<script type="text/javascript">
+			var addthis_config = {
+      	data_ga_property: 'UA-22995010-23',
+      	data_ga_social: true
+   		};
+		</script>
 		<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5a18345cba3f0930"></script>
 		<?php
 	}
+
+	/* END ANALYTICS */
 
 }
 
