8e2c57aa3ede1f4d505cf88409aff0fd17e01d42
Edwin Mah <me@edwinmah.com>
2018-04-16 16:04:34 -0400

    Add polyfill for NodeList.prototype.forEach

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -217,6 +217,17 @@ exports.default = function () {
       }
     });
   }
+
+  // NodeList.prototype.forEach
+  // https://developer.mozilla.org/en-US/docs/Web/API/NodeList/forEach#Polyfill
+  if (window.NodeList && !NodeList.prototype.forEach) {
+    NodeList.prototype.forEach = function (callback, thisArg) {
+      thisArg = thisArg || window;
+      for (var i = 0; i < this.length; i++) {
+        callback.call(thisArg, this[i], i, this);
+      }
+    };
+  }
 }();
 
 },{"core-js/fn/promise":47}],2:[function(require,module,exports){
